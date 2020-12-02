0e9e5801eeec1b57fc284c2441b86009358b01b7
Angel Abraham <angel-07061@hotmail.com>
2020-12-01 20:28:44 -0600

    Commit carfront

--- src/rum.js
+++ src/rum.js
@@ -1,14 +1,14 @@
+export default apm;
+
 import { init as initApm } from '@elastic/apm-rum'
 var apm = initApm({
-  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'carfront',
-  // Set the version of your application
-  // Used on the APM Server to find the right sourcemap
-  serviceVersion: '0.90',
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://5fea15b641064bfa800ef7591069c865.apm.us-central1.gcp.cloud.es.io:443',
-  // For distributed tracing to different origin (CORS)
-  // distributedTracingOrigins: ['http://localhost:8080'],
+ // Establece el nombre de servicio requerido (caracteres permitidos: a-z, A-Z, 0-9, -, _ y espacio)
+ serviceName: 'carfront',
+ // Configura la versión de tu aplicación
+ // Se usa en el servidor de APM para encontrar el mapa fuente correcto
+ serviceVersion: '0.90',
+ // Configura la URL del servidor de APM (predeterminado: http://localhost:8200)
+ serverUrl: 'https://aba7c3d90b0b4820b05b0a9df44c096d.apm.us-central1.gcp.cloud.es.io:443',
+ // distributedTracingOrigins: ['http://localhost:8080'],
 })
-
 export default apm;
